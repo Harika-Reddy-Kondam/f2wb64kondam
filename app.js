5d08cded74cfd096d78ca14a0e0f29ac0cc6f953
Harika-Reddy-Kondam <69505699+Harika-Reddy-Kondam@users.noreply.github.com>
2022-11-01 18:44:49 -0500

    Added gridbuild

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var novelRouter = require('./routes/novel');
+var novelRouter = require('./routes/gridbuild');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/novel', novelRouter);
+app.use('/gridbuild', novelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
